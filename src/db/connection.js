a422e1974c18edfa17a3633c10311d9f39deea97
Syed Abbas Ali <110466727+Syed-Abbas-Ali@users.noreply.github.com>
2024-10-07 17:59:10 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,13 +3,9 @@ import logger from "../logger/index.js";
 
 export const mongoConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         logger.info("Connection is established");
     } catch (err) {
         logger.error(`Error is: ${err}`);
     }
 };
-
